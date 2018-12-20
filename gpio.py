5248943075bb2f7877a813def7f5fe84d19d2482
Yohey Funada <dev@fulufulu.com>
2018-12-20 22:39:53 +0900

    test

--- gpio.py
+++ gpio.py
@@ -1,18 +1,25 @@
-# coding: UTF-8
-import RPi.GPIO as GPIO  #GPIOにアクセスするライブラリをimportします。
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import RPi.GPIO as GPIO
 import time
 
+def main():
+  # 使用する出力ピン(GPIO)
+  pin_num = 14
 
-GPIO.setmode(GPIO.BCM)  #GPIOへアクセスする番号をBCMの番号で指定することを宣言します。
-GPIO.setup(15,GPIO.OUT) #BCMの15番ピン、物理的には10番ピンを出力に設定します。
-GPIO.setup(2,GPIO.IN)   #BCM 2番ピンを入力に設定します。
+  # GPIOピンの定義
+  GPIO.setmode(GPIO.BCM)          # GPIO番号で指定
+  GPIO.setup(pin_num, GPIO.OUT)   # 出力モード
 
-try:
-        while True:
-                if GPIO.input(2) == GPIO.LOW:
-                        GPIO.output(15,GPIO.HIGH)
-                else:
-                        GPIO.output(15,GPIO.LOW)
-                time.sleep(0.1)
-except KeyboardInterrupt:
-        GPIO.cleanup()
+  # 点滅を10回繰り返し
+  for i in range(10):
+    GPIO.output(pin_num, GPIO.HIGH) # 点灯
+    time.sleep(2)                   # 2秒待機
+    GPIO.output(pin_num, GPIO.LOW)  # 消灯
+    time.sleep(2)                   # 2秒待機
+
+  # GPIOピンの設定解除
+  GPIO.cleanup()
+
+if __name__ == "__main__":
+    main()

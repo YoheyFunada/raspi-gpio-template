5d2dcedf197c10cca506c30278bdea579703ad03
Yohey Funada <dev@fulufulu.com>
2018-12-20 22:51:19 +0900

    print

--- gpio.py
+++ gpio.py
@@ -8,15 +8,19 @@ def main():
   pin_num = 14
 
   # GPIOピンの定義
-  GPIO.setmode(GPIO.BCM)          # GPIO番号で指定
-  GPIO.setup(pin_num, GPIO.OUT)   # 出力モード
+  # GPIO番号で指定
+  GPIO.setmode(GPIO.BCM)
+  # 出力モード
+  GPIO.setup(pin_num, GPIO.OUT)
 
-  # 点滅を10回繰り返し
+  # 点滅を100回繰り返し
   for i in range(100):
     GPIO.output(pin_num, GPIO.HIGH) # 点灯
-    time.sleep(1)                   # 2秒待機
-    GPIO.output(pin_num, GPIO.LOW)  # 消灯
-    time.sleep(1)                   # 2秒待機
+    print("HIGH")
+    time.sleep(1)                   # 1秒待機
+    GPIO.output(pin_num, GPIO.LOW)
+    print("LOW")  # 消灯
+    time.sleep(1)                   # 1秒待機
 
   # GPIOピンの設定解除
   GPIO.cleanup()
